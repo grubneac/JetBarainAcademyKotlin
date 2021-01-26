087325abe90132912948ce9149fe8956cf35ab38
Alexandru.Grubneac <grubneac@gmail.com>
2021-01-26 11:24:43 +0200

    Stage 4/8: Add subtractions
    At this stage, your calculator should support the addition + and subtraction - operators.
    
    The program must calculate expressions like these: 4 + 6 - 8, 2 - 3 - 4, and so on. It must support both unary and binary minus operators. If the user has entered several same operators following each other, the program still should work (like Java or Python REPL).
    
    Consider that the even number of minuses gives a plus, and the odd number of minuses gives a minus! Look at it this way: 2 -- 2 equals 2 - (-2) equals 2 + 2.
    
    Modify the result of the /help command to explain these operations.
    
    Decompose your program using methods to make it easy to understand and edit later.

--- src/main/kotlin/calculator/smartCalculator.kt
+++ src/main/kotlin/calculator/smartCalculator.kt
@@ -4,7 +4,7 @@ import java.util.*
 
 fun main() {
     val scanner = Scanner(System.`in`)
-    var arrNum: List<Int>
+    var arrNum: List<String>
     var str: String
     while (true) {
         str = scanner.nextLine()
@@ -14,16 +14,51 @@ fun main() {
                 println("Bye!")
                 break
             }
-            ("/help".equals(str)) -> println("The program calculates the sum of numbers")
+            ("/help".equals(str)) -> println(
+                "The program must calculate expressions like these:" +
+                        " 4 + 6 - 8, 2 - 3 - 4, and so on. It must support both unary and binary minus operators. " +
+                        "If the user has entered several same operators following each other, the program still " +
+                        "should work (like Java or Python REPL).\n" +
+                        "\n" +
+                        "Consider that the even number of minuses gives a plus, and the odd number of minuses gives " +
+                        "a minus! Look at it this way: 2 -- 2 equals 2 - (-2) equals 2 + 2."
+            )
             (str.isBlank()) -> continue
             else -> {
-                arrNum = str.split(" ").map { it.toInt() }
+                arrNum = str.split(" ")
 
                 var sum = 0
-                for (cur in arrNum)
-                    sum += cur
+                var plusMinus = true
+                for (cur in arrNum) {
+                    when {
+                        (isNum(cur)) -> {
+                            if (plusMinus) sum += cur.toInt()
+                            else sum -= cur.toInt()
+                        }
+                        (cur.isBlank()) -> continue
+                        else -> plusMinus = getSign(cur)
+                    }
+                }
                 println(sum)
             }
         }
+    }
+}
+
+fun getSign(str: String): Boolean {
+    val charArr = str.toCharArray()
+    var flag = true
+    for (ch in charArr) {
+        if (ch == '-') flag = !flag
+    }
+    return flag
+}
+
+fun isNum(cur: String): Boolean {
+    return try {
+        cur.toInt()
+        true
+    } catch (e: Exception) {
+        false
     }
 }
